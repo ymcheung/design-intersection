c1d03ff31484f1197fbde259833c0c4e3ea492f3
ymcheung <ymcheung.tw@me.com>
2022-12-11 12:42:30 +0800

    Fix graphql hostname

--- apollo-client.ts
+++ apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: `${process.env.API_HOSTNAME}/graphql/production/default`
+  uri: `${process.env.GRAPHQL_HOSTNAME}/graphql/production/default`
 });
 
 const authLink = setContext((_, { headers }) => {
