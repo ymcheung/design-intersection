89dabc1d2b6a34762fc110fdf596c8dbe6bf03cf
ymcheung <ymcheung.tw@me.com>
2023-12-25 09:15:28 +0800

    Add page speed insight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
-import type { AppProps } from 'next/app';
-import { Analytics } from '@vercel/analytics/react';
+import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function DesignIntersection({ Component, pageProps }: AppProps) {
-  return <>
-    <Component {...pageProps} />
-    <Analytics />
-  </>
-};
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
+    </>
+  );
+}
