c0119b623c7aa9fb0444fa32061faca95a72afb8
ymcheung <ymcheung.tw@me.com>
2024-01-09 20:28:24 +0800

    Fix speed insight import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import type { AppProps } from "next/app";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
+import { useRouter } from "next/router";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function DesignIntersection({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <>
       <Component {...pageProps} />
-      <SpeedInsights />
+      <SpeedInsights route={router.pathname} />
       <Analytics />
     </>
   );
